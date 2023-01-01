6435f41ab86217d435b088acd19e034cc7c37c87
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-01-01 21:08:56 +0300

    Added padding around sign up left div

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -4,7 +4,7 @@ import { assets } from "../Assets/Assets";
 const SignUp = () => {
   return (
     <div className="h-screen">
-      <div className="w-1/2 py-[5%] h-full">
+      <div className="w-1/2 py-[4%] px-[10%] h-full">
         <div className="flex items-center">
           <img src={assets.Logo} alt="logo" />
           <p className="font-bold text-lg dark:text-white ml-2">Maglo</p>
