88d7c0102b30946cff9fa2636b7695b693a64d79
Amoh Prince <81857018+AmohPrince@users.noreply.github.com>
2023-01-01 20:53:32 +0300

    Initial commit, added required libraries and tools.

---
+++ src/Pages/SignUp.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { assets } from "../Assets/Assets";
+
+const SignUp = () => {
+  return (
+    <div className="h-screen">
+      <div className="w-1/2 py-[5%] h-full">
+        <div className="flex items-center">
+          <img src={assets.Logo} alt="logo" />
+          <p className="font-bold text-lg dark:text-white ml-2">Maglo</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
